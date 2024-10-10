8d3cb91f326bb02657359339ba3e645ff0068f6f
Selina Liu <selinaliu@MacBook-Air-7.local>
2024-10-09 17:02:09 -0700

    changed jade to pug

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use('/', apiRoutes);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
